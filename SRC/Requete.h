c2f67754d2e7fe79981c337061a5b1caa14d39b3
IICH <165923196+IICH1@users.noreply.github.com>
2025-01-23 22:34:30 +0100

    Update Requete.h

--- SRC/Requete.h
+++ SRC/Requete.h
@@ -6,26 +6,33 @@
     e-mail               : $EMAIL$
 *************************************************************************/
 //---------- Interface de la classe <Requete> (fichier Requete.h) ----------------
-#if ! defined ( Requete_H )
+#if !defined(Requete_H)
 #define Requete_H
 
 //--------------------------------------------------- Interfaces utilisées
-#include<cstring>
-#include<fstream>
+#include <cstring>
+#include <fstream>
 using namespace std;
 //------------------------------------------------------------- Constantes
 
 //------------------------------------------------------------------ Types
-typedef struct {
+struct Date
+{
     int jour;
     string mois;
     int annee;
-}Date;
-typedef struct {
     int heure;
     int minute;
     int seconde;
-}Temps;
+    string diffGMT;
+};
+
+struct requeteHTTP
+{
+    string action;
+    string url;
+    string http_version;
+};
 //------------------------------------------------------------------------
 // Rôle de la classe <Requete>
 //
@@ -33,54 +40,52 @@ typedef struct {
 //------------------------------------------------------------------------
 class Requete
 {
-//----------------------------------------------------------------- PUBLIC
-friend class Lecture;
+    //----------------------------------------------------------------- PUBLIC
+    friend class Lecture;
+
 public:
-//----------------------------------------------------- Méthodes publiques
-    string GetExtension()const;
+    //----------------------------------------------------- Méthodes publiques
+    string GetExtension() const;
     // type Méthode ( liste des paramètres );
     // Mode d'emploi :
     //
     // Contrat :
     //
-//------------------------------------------------- Surcharge d'opérateurs
-    Requete & operator = ( const Requete & unRequete );
+    //------------------------------------------------- Surcharge d'opérateurs
+    Requete &operator=(const Requete &unRequete);
     // Mode d'emploi :
     //
     // Contrat :
     //
-//-------------------------------------------- Constructeurs - destructeur
-    Requete ( const Requete & unRequete );
+    //-------------------------------------------- Constructeurs - destructeur
+    Requete(const Requete &unRequete);
     // Mode d'emploi (constructeur de copie) :
     //
     // Contrat :
     //
-    Requete (string un_IP_Client, string un_User_Id, string un_User_Name, Date une_Date,Temps un_Temps,
-    string une_Time_Zone, string un_Type_Query, string un_URL, int un_Status, int une_Qte, string un_Referer );
+    Requete(string mAdresseIP, string mLogName, string mUserName, Date mDate, requeteHTTP mRHTTP, int mStatus, int mQte, string mReferer, string mClientID);
     // Mode d'emploi :
     //
     // Contrat :
     //
-    virtual ~Requete ( );
+    virtual ~Requete();
     // Mode d'emploi :
     //
     // Contrat :
     //
-//------------------------------------------------------------------ PRIVE
+    //------------------------------------------------------------------ PRIVE
 protected:
-//----------------------------------------------------- Méthodes protégées
-//----------------------------------------------------- Attributs protégés
-string IP_Client;
-string User_Id;
-string User_Name;
-Date Date;
-Temps Time;
-string Time_Zone;
-string Type_Query;
-string URL;
-int Status;
-int Qte;
-string Referer;
+    //----------------------------------------------------- Méthodes protégées
+    //----------------------------------------------------- Attributs protégés
+    string mAdresseIP;
+    string mLogName;
+    string mUserName;
+    Date mDate;
+    requeteHTTP mRHTTP;
+    int mStatus;
+    int mQte;
+    string mReferer;
+    string mClientID;
 };
 //-------------------------------- Autres définitions dépendantes de <Requete>
 #endif // Requete_H
