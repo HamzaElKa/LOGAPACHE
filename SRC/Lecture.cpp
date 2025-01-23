2c489e2b0c42ab6a5262723e89dac7582cf1c2a3
IICH <165923196+IICH1@users.noreply.github.com>
2025-01-23 23:40:12 +0100

    Update Lecture.cpp

--- SRC/Lecture.cpp
+++ SRC/Lecture.cpp
@@ -7,92 +7,99 @@
 
 using namespace std;
 
-Lecture::Lecture(const string& fluxFichier, const string& baseLocale) : mBaseLocale(baseLocale)
+Lecture::Lecture(const string &fluxFichier, const string &baseLocale) : mBaseLocale(baseLocale)
 {
-    mFluxFichier.open(fluxFichier);
-    if (!mFluxFichier) {
-        cerr << "Erreur, impossible d'ouvrir le fichier !" << endl;
-        exit(EXIT_FAILURE);
-    }
+	mFluxFichier.open(fluxFichier);
+	if (!mFluxFichier)
+	{
+		cerr << "Erreur, impossible d'ouvrir le fichier !" << endl;
+		exit(EXIT_FAILURE);
+	}
 }
 
-Lecture::~Lecture() {
-    if (mFluxFichier.is_open()) {
-        mFluxFichier.close();
-    }
+Lecture::~Lecture()
+{
+	if (mFluxFichier.is_open())
+	{
+		mFluxFichier.close();
+	}
 }
 
-vector<Requete> Lecture::Lire() {
-    vector<Requete> requetes;
-    string ligne;
+vector<Requete> Lecture::Lire()
+{
+	vector<Requete> requetes;
+	string ligne;
 
-    while (getline(mFluxFichier, ligne)) {
-        stringstream ss(ligne);
-        string temp;
+	while (getline(mFluxFichier, ligne))
+	{
+		stringstream ss(ligne);
+		string temp;
 
-        string adresseIP;
-        getline(ss, adresseIP, ' ');
+		string adresseIP;
+		getline(ss, adresseIP, ' ');
 
-        string logName;
-        getline(ss, logName, ' ');
+		string logName;
+		getline(ss, logName, ' ');
 
-        string userName;
-        getline(ss, userName, ' ');
+		string userName;
+		getline(ss, userName, ' ');
 
-        Date date;
-        getline(ss, temp, '['); 
-        getline(ss, temp, '/');
-        date.jour = stoi(temp);
-        getline(ss, date.mois, '/');
-        getline(ss, temp, ':');
-        date.annee = stoi(temp);
-        getline(ss, temp, ':');
-        date.heure = stoi(temp);
-        getline(ss, temp, ':');
-        date.minute = stoi(temp);
-        getline(ss, temp, ' ');
-        date.seconde = stoi(temp);
-        getline(ss, date.diffGMT, ']');
+		Date date;
+		getline(ss, temp, '[');
+		getline(ss, temp, '/');
+		date.jour = stoi(temp);
+		getline(ss, date.mois, '/');
+		getline(ss, temp, ':');
+		date.annee = stoi(temp);
+		getline(ss, temp, ':');
+		date.heure = stoi(temp);
+		getline(ss, temp, ':');
+		date.minute = stoi(temp);
+		getline(ss, temp, ' ');
+		date.seconde = stoi(temp);
+		getline(ss, date.diffGMT, ']');
 
-        getline(ss, temp, ' ');  
-        getline(ss, temp, '"');  
+		getline(ss, temp, ' ');
+		getline(ss, temp, '"');
 
-        requeteHTTP rHTTP;
-        getline(ss, rHTTP.action, ' ');
-        getline(ss, rHTTP.url, ' ');
-        getline(ss, rHTTP.http_version, '"');  
+		requeteHTTP rHTTP;
+		getline(ss, rHTTP.action, ' ');
+		getline(ss, rHTTP.url, ' ');
+		getline(ss, rHTTP.http_version, '"');
 
-        getline(ss, temp, ' ');  
+		getline(ss, temp, ' ');
 
-        int status;
-        getline(ss, temp, ' ');
-        status = stoi(temp);
+		int status;
+		getline(ss, temp, ' ');
+		status = stoi(temp);
 
-        int qte;
-        getline(ss, temp, ' ');
-        qte = stoi(temp);
+		int qte;
+		getline(ss, temp, ' ');
+		qte = stoi(temp);
 
-        string referer;
-        getline(ss, temp, '"');  
-        getline(ss, referer, '"'); 
+		string referer;
+		getline(ss, temp, '"');
+		getline(ss, referer, '"');
 
-        size_t position = referer.find(mBaseLocale);
-        if (position != string::npos) {
-            referer.erase(position, mBaseLocale.length());
-        }
+		size_t position = referer.find(mBaseLocale);
+		if (position != string::npos)
+		{
+			referer.erase(position, mBaseLocale.length());
+		}
 
-        string clientID;
-        getline(ss, temp, '"');  
-        getline(ss, clientID, '"'); 
+		string clientID;
+		getline(ss, temp, '"');
+		getline(ss, clientID, '"');
 
-        Requete r(adresseIP, logName, userName, date, rHTTP, status, qte, referer, clientID);
-        requetes.push_back(r);
-    }
+		Requete r(adresseIP, logName, userName, date, rHTTP, status, qte, referer, clientID);
+		requetes.push_back(r);
+	}
 
-    if (requetes.empty()) {
-        cerr << "Erreur, aucune ligne valide n'a été trouvée !" << endl;
-        exit(EXIT_FAILURE);
-    }
+	if (requetes.empty())
+	{
+		cerr << "Erreur, aucune ligne valide n'a été trouvée !" << endl;
+		exit(EXIT_FAILURE);
+	}
 
-    return requetes;
+	return requetes;
 }
