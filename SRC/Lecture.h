36949e4620110ba08805487ec352242094c369e8
ybellargui <y.bellargui@icloud.com>
2025-02-04 17:15:31 +0100

    Update Lecture.h

--- SRC/Lecture.h
+++ SRC/Lecture.h
@@ -1,3 +1,12 @@
+/*************************************************************************
+                           main -  description
+                             -------------------
+    début                : 22/01/2025
+    copyright            : (C) 2025 par B3107 - B3110
+    e-mail               : mohammed.iich@insa-lyon.fr / hamza.el-karchouni@insa-lyon.fr / 
+    			   mohamed.lemseffer@insa-lyon.fr / yliess.bellargui@insa-lyon.fr / youssef.erabhaoui@insa-lyon.fr
+*************************************************************************/
+
 #ifndef LECTURE_H
 #define LECTURE_H
 
@@ -6,17 +15,23 @@
 #include "Requete.h"
 #include "Filtrage.h"
 
+// Classe permettant la lecture des requêtes depuis un fichier
 class Lecture
 {
 public:
+    // Constructeur prenant en paramètre le nom du fichier à lire et l'URL de base
     Lecture(const string &fluxFichier, const string &baseLocale);
+    
+    // Destructeur libérant les ressources associées
     ~Lecture();
 
+    // Méthode permettant de lire les requêtes et de les filtrer selon un objet Filtrage
     vector<Requete> Lire(const Filtrage & filtre);
 
 protected:
-    ifstream mFluxFichier;
-    string mBaseLocale;
+    ifstream mFluxFichier; // Flux de fichier pour la lecture
+    string mBaseLocale; // URL de base utilisée pour la lecture et le traitement des requêtes
 };
 
 #endif // LECTURE_H
+
