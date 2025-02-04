c24696064ee9c7512e26cf520f09059bc2ad8ed9
IICH <165923196+IICH1@users.noreply.github.com>
2025-02-04 17:32:45 +0100

    Update Classement.cpp

--- SRC/Classement.cpp
+++ SRC/Classement.cpp
@@ -1,18 +1,38 @@
-#include <iostream>
-using namespace std;
+/*************************************************************************
+                           Classement  -  Classement des requêtes
+                             -------------------
+    début                : $DATE$
+    copyright            : (C) $YEAR$ par $AUTHOR$
+    e-mail               : $EMAIL$
+*************************************************************************/
 
+//---------- Réalisation de la classe <Classement> (fichier Classement.cpp) ------------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+using namespace std;
+#include <iostream>
+
+//------------------------------------------------------ Include personnel
 #include "Classement.h"
+
+//------------------------------------------------------------- Constantes
+
+//----------------------------------------------------------------- PUBLIC
+
+//----------------------------------------------------- Méthodes publiques
 
 void Classement::Ajouter(const vector<Requete> & unVecRequetes)
 {
-	if (unVecRequetes.empty())
-	{
-		cerr << "Erreur : Vous n'avez aucune requete" << endl;
-		return;
-	}
+    if (unVecRequetes.empty())
+    {
+        cerr << "Erreur : Vous n'avez aucune requete" << endl;
+        return;
+    }
 
-	vector<Requete>::const_iterator it;
-	for (it = unVecRequetes.begin(); it != unVecRequetes.end(); ++it)
+    vector<Requete>::const_iterator it;
+    for (it = unVecRequetes.begin(); it != unVecRequetes.end(); ++it)
     {
         string dest = it->GetDestination();
         map<string, int>::iterator classementIt = classement.find(dest);
@@ -29,44 +49,45 @@ void Classement::Ajouter(const vector<Requete> & unVecRequetes)
 
 void Classement::Affichage()
 {
-	if (classement.empty())
-	{
-		cout << "Aucune donnée à afficher." << endl;
-		return;
-	}
+    if (classement.empty())
+    {
+        cout << "Aucune donnée à afficher." << endl;
+        return;
+    }
 
-	vector<pair<string, int>> vec(classement.begin(), classement.end());
+    vector<pair<string, int>> vec(classement.begin(), classement.end());
 
-	sort(vec.begin(), vec.end(),
-		 [](const pair<string, int> &a, const pair<string, int> &b) {
-			 return b.second < a.second;
-		 });
+    sort(vec.begin(), vec.end(),
+         [](const pair<string, int> &a, const pair<string, int> &b) {
+             return b.second < a.second;
+         });
 
-	cout << "Top URLs consultées :" << endl;
-	for (size_t i = 0; i < 10 && i < vec.size(); ++i)
-	{
-		cout << vec[i].first << " (" << vec[i].second << " hits)" << endl;
-	}
+    cout << "Top URLs consultées :" << endl;
+    for (size_t i = 0; i < 10 && i < vec.size(); ++i)
+    {
+        cout << vec[i].first << " (" << vec[i].second << " hits)" << endl;
+    }
 }
 
+//-------------------------------------------- Constructeurs - destructeur
 Classement::Classement()
 {
-	#ifdef MAP
-    	cout << "Appel au constructeur de <Classement>" << endl;
-	#endif
+#ifdef MAP
+    cout << "Appel au constructeur de <Classement>" << endl;
+#endif
 }
 
 Classement::Classement(const Classement & unClassement)
 {
-	#ifdef MAP
-    	cout << "Appel au constructeur de copie de <Classement>" << endl;
-	#endif
-	classement = unClassement.classement;
+#ifdef MAP
+    cout << "Appel au constructeur de copie de <Classement>" << endl;
+#endif
+    classement = unClassement.classement;
 }
 
 Classement::~Classement()
 {
-	#ifdef MAP
-    	cout << "Appel au destructeur de <Classement>" << endl;
-	#endif
+#ifdef MAP
+    cout << "Appel au destructeur de <Classement>" << endl;
+#endif
 }
